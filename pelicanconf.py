43a3a31a706280945b0af0c87b0fbcede2b94317
IRawlings <Iain.Rawlings@gmail.com>
2023-10-19 15:24:09 +0100

    initial commit

---
+++ pelicanconf.py
@@ -0,0 +1,31 @@
+AUTHOR = 'Iain Rawlings'
+SITENAME = 'IainRawlings.com'
+SITEURL = 'IainRawlings.com'
+
+PATH = 'content'
+THEME = "/home/iain/projects/pelican/test/pelican-themes/octopress"
+TIMEZONE = 'Europe/London'
+
+DEFAULT_LANG = 'en'
+LOCALE = 'en_GB.UTF-8'
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'https://getpelican.com/'),
+         ('Python.org', 'https://www.python.org/'),
+         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+         ('You can modify those links in your config file', '#'),
+	 ('makeshift.ninja', 'Makeshift.ninja'))
+
+# Social widget
+SOCIAL = (('LinkedIn', 'https://www.linkedin.com/in/iain-rawlings-37866121/'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
---
+++ publishconf.py
@@ -0,0 +1,22 @@
+# This file is only used if you use `make publish` or
+# explicitly specify it as your config file.
+
+import os
+import sys
+sys.path.append(os.curdir)
+from pelicanconf import *
+
+# If your site is available via HTTPS, make sure SITEURL begins with https://
+SITEURL = 'IainRawlings.com'
+RELATIVE_URLS = True
+
+DEFAULT_DATE_FORMAT = '%d %B %Y'
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+
+DELETE_OUTPUT_DIRECTORY = True
+
+# Following items are often useful when publishing
+
+#DISQUS_SITENAME = ""
+#GOOGLE_ANALYTICS = ""
