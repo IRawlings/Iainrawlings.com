c996ea59afe6f1d64df6598310e7a892a800d70d
IRawlings <Iain.Rawlings@gmail.com>
2024-03-23 14:33:18 +0000

    CI / CD initial testing of bash script for actions required by github.

---
+++ bash.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+# vars:
+OUTPUTDIR=output
+S3_BUCKET=iainrawlings.com
+# Added CLOUDFRONT DIST ID to allow cache invalidation
+CLOUDFRONT_DIST_ID=E1KXVSFIKUXWDG
+
+# generate content using the pelican command
+pelican content
+
+# upload to S3.
+aws s3 cp $OUTPUTDIR/ s3://$S3_BUCKET --recursive
+
+# request to invalidate cloudfront dist
+TEMP=aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DIST_ID --paths "/" | jq -r  '.Invalidation.Id' ;\
+  echo "Invalidating Cloudfront cache..." ;\
+# await confirmation the invalidation has completed and then confirm
+aws cloudfront wait invalidation-completed --id $TEMP --distribution-id $CLOUDFRONT_DIST_ID ;\
+  echo "Invalidation request: $TEMP Completed. Upload Complete & Cache Reset" ;\
